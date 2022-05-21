4ed362ec0ebfefea4114c69b37d884e10c0557b1
aman043 <dheerajkumarr235@gmail.com>
2022-05-21 23:29:56 +0530

    update

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Author, Genre, Book, BookInstance
+from .models import Author, Genre, Book, BookInstance, Language
 
 admin.site.register(Author)
 admin.site.register(Genre)
