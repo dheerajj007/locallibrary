030179669068ca1d36a7340bfc3c5bbab56b91c9
aman043 <dheerajkumarr235@gmail.com>
2022-05-22 09:37:01 +0530

    added template

--- catalog/admin.py
+++ catalog/admin.py
@@ -4,8 +4,13 @@ from django.contrib import admin
 
 from .models import Author, Genre, Book, BookInstance, Language
 
-admin.site.register(Author)
+# admin.site.register(Author)
 admin.site.register(Genre)
 admin.site.register(Book)
 admin.site.register(BookInstance)
 admin.site.register(Language)
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
